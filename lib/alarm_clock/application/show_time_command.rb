d844dca7f36dad3875ab7756791e6b952bead0f0
Fran Iglesias <franiglesias@mac.com>
2023-11-14 20:45:02 +0100

    Show the current time with an emoji

--- lib/alarm_clock/application/show_time_command.rb
+++ lib/alarm_clock/application/show_time_command.rb
@@ -13,3 +13,22 @@ class ShowTimeHandler
     @display.show("#{command.time}:00")
   end
 end
+
+
+class EmojiShowTimeHandler
+  def initialize(display)
+    @display = display
+  end
+
+  def execute(command)
+    raise ArgumentError, 'invalid command' unless command.is_a? ShowTimeCommand
+
+    emojis = [
+      "🕛", "🕐", "🕑", '🕒', "🕓", "🕔", "🕕", "🕖", "🕗", "🕘", "🕙", "🕚"
+    ]
+    pick = command.time % 12
+    emoji = emojis[pick]
+
+    @display.show("#{emoji}")
+  end
+end
