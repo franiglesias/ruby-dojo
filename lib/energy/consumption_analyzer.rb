14225a6f1a775375a1d2f02b220cdfbf26492a45
Fran Iglesias <franiglesias@mac.com>
2023-11-29 13:05:51 +0100

    Separate the aggregation of consumption from the analysis

--- lib/energy/consumption_analyzer.rb
+++ lib/energy/consumption_analyzer.rb
@@ -4,70 +4,68 @@ require "csv"
 Outlier = Struct.new(:office, :consumption, :deviation)
 
 class ConsumptionAnalyzer
-  def initialize
-
-  end
-
-  CONSUMPTIONS_A_YEAR = 12
-
-  def execute(file_name, deviation_factor = 1.4)
-
-    data = obtain_readings(file_name)
-    consumptions = []
-    outliers = []
-    offices = {}
-    data.each do |row|
-      consumptions.append(row["consumption"])
-
-      next if consumptions.size < CONSUMPTIONS_A_YEAR
-
-      officeId = "#{row["office"]}-#{row["year"]}"
-      offices[officeId] = consumptions
-
-      average = average(consumptions)
-      standard_deviation = standard_deviation(consumptions)
-
-      consumptions.each do |consumption|
-        difference = (consumption - average).abs
-        boundary = standard_deviation * deviation_factor
-
-        next unless difference > boundary
-
-        outlier = Outlier.new
-        outlier.office = officeId.split('-')[0].to_i
-        outlier.consumption = consumption
-        outlier.deviation = (consumption - average) / standard_deviation
-
-        outliers.append(outlier)
-      end
-
-      consumptions = []
-    end
-    
-    offices.each do | officeId, consumptions|
+    def initialize
 
     end
-    
-    puts outliers
-    puts "Data sample #{data.size} rows"
-    puts "Found #{outliers.size} outliers"
-    puts "Found #{outliers.size / 300} per office"
-  end
 
-  def obtain_readings(file_name)
-    CSV.parse(File.read(file_name), headers: true, converters: :numeric)
-  end
+    CONSUMPTIONS_A_YEAR = 12
 
-  def standard_deviation(consumptions)
-    Math.sqrt(variance(consumptions))
-  end
+    def execute(file_name, deviation_factor = 1.4)
 
-  def variance(consumptions)
-    sum = consumptions.sum(0.0) { |element| (element - average(consumptions))**2 }
-    sum / (consumptions.size - 1)
-  end
+        data = obtain_readings(file_name)
+        consumptions = []
+        outliers = []
+        offices = {}
+        data.each do |row|
+            consumptions.append(row["consumption"])
 
-  def average(consumptions)
-    consumptions.sum(0.0) / consumptions.size
-  end
+            next if consumptions.size < CONSUMPTIONS_A_YEAR
+
+            officeId = "#{row["office"]}-#{row["year"]}"
+            offices[officeId] = consumptions
+
+            consumptions = []
+        end
+
+        offices.each do |officeId, consumptions|
+            average = average(consumptions)
+            standard_deviation = standard_deviation(consumptions)
+
+            consumptions.each do |consumption|
+                difference = (consumption - average).abs
+                boundary = standard_deviation * deviation_factor
+
+                next unless difference > boundary
+
+                outlier = Outlier.new
+                outlier.office = officeId.split('-')[0].to_i
+                outlier.consumption = consumption
+                outlier.deviation = (consumption - average) / standard_deviation
+
+                outliers.append(outlier)
+            end
+        end
+
+        puts outliers
+        puts "Data sample #{data.size} rows"
+        puts "Found #{outliers.size} outliers"
+        puts "Found #{outliers.size / 300} per office"
+    end
+
+    def obtain_readings(file_name)
+        CSV.parse(File.read(file_name), headers: true, converters: :numeric)
+    end
+
+    def standard_deviation(consumptions)
+        Math.sqrt(variance(consumptions))
+    end
+
+    def variance(consumptions)
+        sum = consumptions.sum(0.0) { |element| (element - average(consumptions)) ** 2 }
+        sum / (consumptions.size - 1)
+    end
+
+    def average(consumptions)
+        consumptions.sum(0.0) / consumptions.size
+    end
 end
