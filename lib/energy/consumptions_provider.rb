6b51e74d0d61f661fe4a7648374cff00526106a6
Fran Iglesias <franiglesias@mac.com>
2023-12-02 18:35:25 +0100

    Introducing strategy pattern with only one strategy

--- lib/energy/consumptions_provider.rb
+++ lib/energy/consumptions_provider.rb
@@ -5,6 +5,10 @@ class ConsumptionsProvider
         @provider = provider
     end
     def from_file(file_name)
-        @provider.from_file(file_name)
+        extension = File.extname(file_name)
+        if extension == ".csv"
+            return @provider.from_file(file_name)
+        end
+        raise NotImplementedError.new , "#{extension} file support not implemented"
     end
 end
