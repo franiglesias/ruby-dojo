00dc96e7da8caea676cadc619a0dc96854a85a1d
Fran Iglesias <franiglesias@mac.com>
2023-12-02 18:47:44 +0100

    Clean code a bit

--- lib/energy/consumptions_provider.rb
+++ lib/energy/consumptions_provider.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 class ConsumptionsProvider
-    def initialize(provider = CsvConsumptionsProvider.new)
-        @provider = provider
+    def initialize
+
     end
     def from_file(file_name)
         extension = File.extname(file_name)
         if extension == ".csv"
-            return @provider.from_file(file_name)
+            provider = CsvConsumptionsProvider.new
+            return provider.from_file(file_name)
         end
         raise NotImplementedError.new , "#{extension} file support not implemented"
     end
